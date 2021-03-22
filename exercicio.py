81bcc19a369ea3f86a9ec5e2fd48bf9e10d4517b
Weslley02 <80173269+Weslley02@users.noreply.github.com>
2021-03-22 16:29:46 -0300

    Add files via upload

---
+++ exercicio.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import os
+
+INPUT_FOLDER = "input"
+OUTPUT_FOLDER = "output"
+
+def in_path(filename):
+    return os.path.join(INPUT_FOLDER, filename)
+
+def bandeirafranca(height):
+    width = 3height//2
+    BLUE = (0, 85, 164)
+    WHITE = (255, 255, 255)
+    RED = (239, 65, 53)
+    image = Image.new("RGB", (width,height), WHITE)
+
+    offset = width//3
+    for x in range(offset):
+        for y in range(height):
+            image.putpixel((x, y), BLUE)
+            image.putpixel((x + 2offset, y), RED)
+    return image
+
+if name == "main":
+    bandeira = bandeirafranca(700)
+    bandeira.show()
